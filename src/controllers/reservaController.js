caa30c52aaf6298015ea959f7db7857841209b17
Maria Luisa <117184776+marialuisaalonso850@users.noreply.github.com>
2024-03-31 23:34:53 -0500

    Update reservaController.js

--- src/controllers/reservaController.js
+++ src/controllers/reservaController.js
@@ -1,9 +1,11 @@
 const Reserva = require('../models/reserva.js');
 const Parqueadero = require('../models/post.js');
-const sendConfirmationReserva= require('../routes/correoReserva.js')
+const sendConfirmationReserva= require('../routes/correoReserva.js');
+const sendSalidaReserva = require('../routes/correoSalida.js');
 
 async function createReserva(req, res) {
   try {
+    const userId = req.userId;
     const reserva = new Reserva({
       date: req.body.date,
       time: req.body.time,
@@ -11,16 +13,14 @@ async function createReserva(req, res) {
       correo: req.body.correo,
       placa: req.body.placa,
       telefono: req.body.telefono,  
-      parqueadero: req.body.parqueaderoId 
-      
+      parqueadero: req.body.parqueaderoId, 
+      userId // Asigna el userId al campo userId del post
     });
 
     await reserva.save();
 
     // Disminuye el número de puestos disponibles en el parqueadero correspondiente
     await Parqueadero.findByIdAndUpdate(req.body.parqueaderoId, { $inc: { puestos: -1 } });
-    
-
     
     
     await sendConfirmationReserva(reserva.nombre,
@@ -84,6 +84,28 @@ async function deleteReserva(req, res) {
     
     // Incrementar el número de puestos disponibles en el parqueadero correspondiente
     await Parqueadero.findByIdAndUpdate(reserva.parqueadero, { $inc: { puestos: 1 } });
+
+    await sendSalidaReserva(reserva.nombre,
+      reserva.date,
+      reserva.time,
+      reserva.correo,
+      reserva.telefono,
+      reserva.placa,
+    );
+    
+    res.send(reserva);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+async function cancelReserva(req, res) {
+  try {
+    const reserva = await Reserva.findByIdAndDelete(req.params.id);
+    if (!reserva) return res.status(404).send("Reserva not found");
+    
+    // Incrementar el número de puestos disponibles en el parqueadero correspondiente
+    await Parqueadero.findByIdAndUpdate(reserva.parqueadero, { $inc: { puestos: 1 } });
     
     res.send(reserva);
   } catch (error) {
@@ -96,5 +118,6 @@ module.exports = {
   getAllReservas,
   updateReserva,
   getReservaById,
-  deleteReserva
+  deleteReserva,
+  cancelReserva
 };
